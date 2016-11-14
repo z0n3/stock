bb743b115ce5fd8e455c386a28a0512c049573c7
Jason <jason@jason>
2016-11-14 21:46:53 +0800

    update output jasonshort

--- main/stock.py
+++ main/stock.py
@@ -15,7 +15,7 @@ from core import depickle_stock_list
 def timetosleep():
     
     curTime = datetime.now()
-    desTime = curTime.replace(hour=21, minute=0, second=0, microsecond=0)
+    desTime = curTime.replace(hour=20, minute=0, second=0, microsecond=0)
     delta = desTime - curTime
     skipSeconds =  delta.total_seconds()
     return skipSeconds
