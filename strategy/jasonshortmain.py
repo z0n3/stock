56a1e58787c3a9258ee537b38018331e6d0ea55e
Jason <jason@jason>
2016-12-01 09:01:26 +0800

    small edit

--- strategy/jasonshortmain.py
+++ strategy/jasonshortmain.py
@@ -57,7 +57,7 @@ def runjasonshort(logfile):
                 a.test()
                 b=a.stockdayline[a.stockdayline['yz']==True]['yz']
                 b=b.reset_index()
-                b['span'] = (today - b['date']) < datetime.timedelta(days=14)
+                b['span'] = (today - b['date']) < datetime.timedelta(days=2)
                 c=b[b['span']==True]['date']
                 if (len(c)>0):
                     print(code+','+str(c[0])[:11]+'\n')
