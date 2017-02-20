cc78481aa60337dc2827307221bcb1bc06d313ff
z0n3 <z0n3@z0n3>
2017-02-20 17:03:16 +0800

    add minchoice

---
+++ stock.py
@@ -0,0 +1,46 @@
+#import struct
+from StockClass import Stock
+from conf import tdx_fz_line_file
+from core import depickle_stock_list
+from core import get_stock_market
+
+generaldir = tdx_fz_line_file   #new_zx_allin1
+Sse_lday = generaldir + r"\sh\lday\sh"
+Sze_lday = generaldir + r"\sz\lday\sz"
+
+
+def minchoice(logfile):
+    print('minchoice output' + ':\n')
+    logfile.write('minchoice output' + ':\n')
+    for code in depickle_stock_list():
+        market = get_stock_market(code)
+        if market in ['sh','sz']:
+            try:
+                if market == 'sh':
+                    choosedir = Sse_lday
+                else:
+                    choosedir = Sze_lday
+                    
+                stk=Stock(code)
+                stk.readldaysim(choosedir,20)
+                #print(stk.ldayvalue)
+                upcount = 0
+                for n in range(15):
+                    if stk.limitup( n ): upcount += 1
+                #print(upcount)
+                if upcount < 3: return False
+                
+                gd = stk.gaodian(0,14)
+                #print(stk.ldayvalue[0]["Close"] / gd)
+                if stk.ldayvalue[0]["Close"] / gd < 0.85:
+                    
+                    print(code)
+                    logfile.write(code+'\n')
+    
+            except Exception as e:
+                print(e)
+
+if __name__ == "__main__":
+    logfile=open('minchoice.log','w')
+    minchoice(logfile)        
+    logfile.close()
